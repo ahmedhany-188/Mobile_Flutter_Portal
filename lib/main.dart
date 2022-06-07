d4ad9bbfe069d6c01884a5af0c295cd35ce86275
Ahmed.Elghandour <ahmed.elghandour@hassanallam.com>
2022-06-07 14:45:27 +0200

    commit - commit update Firebase provider with adding extensions and main added firebase crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -53,6 +54,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform, //This line is necessary
   );
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
   HydratedBlocOverrides.runZoned(
     () => runApp(MyApp(appRouter: AppRouter(), connectivity: Connectivity())),
     storage: storage,
