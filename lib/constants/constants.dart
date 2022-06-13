918c7d441ea7dda6fc7c3b74c639303306875717
Ahmed.Elghandour <ahmed.elghandour@hassanallam.com>
2022-06-13 16:24:10 +0200

    commit - commit vacation and permission and business mission

---
+++ lib/constants/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+class GlobalConstants{
+
+  static final DateFormat dateFormatViewed = DateFormat("EEEE dd-MM-yyyy");
+  static final DateFormat dateFormatServer = DateFormat("yyyy-MM-dd'T'HH:mm:ss");
+}
