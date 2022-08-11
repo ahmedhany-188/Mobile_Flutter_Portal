6c075bfe01eaa2329ba38b4798cafad0e1b886ac
Ahmed.Elghandour <elghandourahmed@gmail.com>
2022-08-11 17:00:09 +0200

    commit update my request ticket with sizer

--- lib/screens/my_requests_screen/my_requests_ticket_widget.dart
+++ lib/screens/my_requests_screen/my_requests_ticket_widget.dart
@@ -24,179 +24,181 @@ class MyRequestsItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ConditionalBuilder(
-          condition: listFromRequestScreen.isNotEmpty,
-          builder: (context) => ListView.separated(
-            physics: const BouncingScrollPhysics(),
-            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-            itemBuilder: (context, index) => Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Container(
-                padding: const EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15.0),
-                  gradient: const LinearGradient(
-                      colors: [
-                        Color(0xFF1a4c78),
-                        Color(0xFF3772a6),
-                      ],
-                      begin: Alignment.bottomLeft,
-                      end: Alignment.topRight,
-                      tileMode: TileMode.clamp),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.3),
-                      spreadRadius: 5,
-                      blurRadius: 3,
-                      offset: const Offset(0, 2), // changes position of shadow
+    return Sizer(builder: (c,o,d){return ConditionalBuilder(
+      condition: listFromRequestScreen.isNotEmpty,
+      builder: (context) => ListView.separated(
+        physics: const BouncingScrollPhysics(),
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        itemBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Container(
+            padding: const EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+              gradient: const LinearGradient(
+                  colors: [
+                    Color(0xFF1a4c78),
+                    Color(0xFF3772a6),
+                  ],
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topRight,
+                  tileMode: TileMode.clamp),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  spreadRadius: 5,
+                  blurRadius: 3,
+                  offset: const Offset(0, 2), // changes position of shadow
+                ),
+              ],
+            ),
+            child: InkWell(
+              onTap: () {
+                _pushForRequestDetail(context, listFromRequestScreen[index]);
+              },
+              child: SizedBox(
+                //TODO change size .h
+                height: 20.0.h,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Flexible(
+                      fit: FlexFit.loose,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '${listFromRequestScreen[index].serviceName}'.trim(),
+                              style: const TextStyle(
+                                fontSize: 17.0,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                              ),
+                              maxLines: 1,
+                              // overflow: TextOverflow.ellipsis,
+                            ),
+                            Text(
+                              '# ${listFromRequestScreen[index].requestNo}'.trim(),
+                              style: const TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                              ),
+                              maxLines: 1,
+                              // overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 5, bottom: 5),
+                      child: Container(
+                        width: double.infinity,
+                        // Todo  .h height
+                        height: 0.2.h,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Container(
+                      clipBehavior: Clip.none,
+                      alignment: Alignment.center,
+                      child: Column(
+                        children: [
+                          // Text(
+                          //   '${listFromRequestScreen[index].statusName}'
+                          //       .trim(),
+                          //   style: const TextStyle(
+                          //     fontSize: 14.0,
+                          //     color: Colors.white,
+                          //   ),
+                          //   maxLines: 2,
+                          //   overflow: TextOverflow.ellipsis,
+                          //   textAlign: TextAlign.center,
+                          // ),
+                          MyRequestStatus(listFromRequestScreen[index].statusName.toString(), context),
+                          Text(
+                            GlobalConstants.dateFormatViewed.format(GlobalConstants.dateFormatServer.parse(listFromRequestScreen[index].reqDate ?? "")),
+                            style: const TextStyle(
+                              fontSize: 13.0,
+                              color: Colors.white,
+                            ),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
-                child: InkWell(
-                  onTap: () {
-                    _pushForRequestDetail(context, listFromRequestScreen[index]);
-                  },
-                  child: SizedBox(
-                    height: 20.0.h,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Flexible(
-                          fit: FlexFit.loose,
-                          child: Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  '${listFromRequestScreen[index].serviceName}'.trim(),
-                                  style: const TextStyle(
-                                    fontSize: 17.0,
-                                    fontWeight: FontWeight.w600,
-                                    color: Colors.white,
-                                  ),
-                                  maxLines: 1,
-                                  // overflow: TextOverflow.ellipsis,
-                                ),
-                                Text(
-                                  '# ${listFromRequestScreen[index].requestNo}'.trim(),
-                                  style: const TextStyle(
-                                    fontSize: 16.0,
-                                    fontWeight: FontWeight.w600,
-                                    color: Colors.white,
-                                  ),
-                                  maxLines: 1,
-                                  // overflow: TextOverflow.ellipsis,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 5, bottom: 5),
-                          child: Container(
-                            width: double.infinity,
-                            height: 0.2.h,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Container(
-                          clipBehavior: Clip.none,
-                          alignment: Alignment.center,
-                          child: Column(
-                            children: [
-                              // Text(
-                              //   '${listFromRequestScreen[index].statusName}'
-                              //       .trim(),
-                              //   style: const TextStyle(
-                              //     fontSize: 14.0,
-                              //     color: Colors.white,
-                              //   ),
-                              //   maxLines: 2,
-                              //   overflow: TextOverflow.ellipsis,
-                              //   textAlign: TextAlign.center,
-                              // ),
-                              MyRequestStatus(listFromRequestScreen[index].statusName.toString(), context),
-                              Text(
-                                GlobalConstants.dateFormatViewed.format(GlobalConstants.dateFormatServer.parse(listFromRequestScreen[index].reqDate ?? "")),
-                                style: const TextStyle(
-                                  fontSize: 13.0,
-                                  color: Colors.white,
-                                ),
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                                textAlign: TextAlign.center,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
               ),
             ),
-            separatorBuilder: (context, index) => Padding(
-              padding: const EdgeInsetsDirectional.only(start: 20.0, end: 20.0),
-              child: Container(
-                width: double.infinity,
-                height: 0.2.h,
-                color: Colors.grey[300],
-              ),
-            ),
-            itemCount: listFromRequestScreen.length,
           ),
-              // (context) =>
-              // GridView.builder(
-              //   keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior
-              //       .onDrag,
-              //   shrinkWrap: true,
-              //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              //     crossAxisCount: 1,
-              //     // childAspectRatio: (1 / .4),
-              //     mainAxisExtent: 90, // here set custom Height You Want
-              //     // width between items
-              //     crossAxisSpacing: 30,
-              //     // height between items
-              //     mainAxisSpacing: 30,
-              //   ),
-              //   itemCount: listFromRequestScreen.length,
-              //   itemBuilder: (BuildContext context, int index) {
-              //     MyRequestsModelData myRequestsModelData = listFromRequestScreen[index];
-              //     return SizedBox(
-              //       width: double.infinity,
-              //       child: InkWell(
-              //
-              //         onTap: () {
-              //           _pushForRequestDetail(context,myRequestsModelData);
-              //         },
-              //         child: Container(
-              //           height: 50,
-              //           alignment: Alignment.center,
-              //           decoration: BoxDecoration(
-              //             borderRadius: BorderRadius.circular(20),
-              //             color: Colors.white,
-              //           ),
-              //           child: Column(
-              //             mainAxisAlignment: MainAxisAlignment.center,
-              //             children: [
-              //               Text(myRequestsModelData.serviceName.toString(),
-              //                   style: const TextStyle(
-              //                       color: Colors.black, fontSize: 18)),
-              //               MyRequestStatus(myRequestsModelData.statusName.toString(), context),
-              //               Text(myRequestsModelData.rDate.toString(),
-              //                   style: const TextStyle(
-              //                       color: Colors.black, fontSize: 16)),
-              //             ],
-              //           ),
-              //         ),
-              //       ),
-              //     );
-              //   },
-              // ),
-          fallback: (context) =>
-          const Center(child: LinearProgressIndicator()),
-        );
+        ),
+        separatorBuilder: (context, index) => Padding(
+          padding: const EdgeInsetsDirectional.only(start: 20.0, end: 20.0),
+          child: Container(
+            width: double.infinity,
+            height: 0.2.h,
+            color: Colors.grey[300],
+          ),
+        ),
+        itemCount: listFromRequestScreen.length,
+      ),
+      // (context) =>
+      // GridView.builder(
+      //   keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior
+      //       .onDrag,
+      //   shrinkWrap: true,
+      //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      //     crossAxisCount: 1,
+      //     // childAspectRatio: (1 / .4),
+      //     mainAxisExtent: 90, // here set custom Height You Want
+      //     // width between items
+      //     crossAxisSpacing: 30,
+      //     // height between items
+      //     mainAxisSpacing: 30,
+      //   ),
+      //   itemCount: listFromRequestScreen.length,
+      //   itemBuilder: (BuildContext context, int index) {
+      //     MyRequestsModelData myRequestsModelData = listFromRequestScreen[index];
+      //     return SizedBox(
+      //       width: double.infinity,
+      //       child: InkWell(
+      //
+      //         onTap: () {
+      //           _pushForRequestDetail(context,myRequestsModelData);
+      //         },
+      //         child: Container(
+      //           height: 50,
+      //           alignment: Alignment.center,
+      //           decoration: BoxDecoration(
+      //             borderRadius: BorderRadius.circular(20),
+      //             color: Colors.white,
+      //           ),
+      //           child: Column(
+      //             mainAxisAlignment: MainAxisAlignment.center,
+      //             children: [
+      //               Text(myRequestsModelData.serviceName.toString(),
+      //                   style: const TextStyle(
+      //                       color: Colors.black, fontSize: 18)),
+      //               MyRequestStatus(myRequestsModelData.statusName.toString(), context),
+      //               Text(myRequestsModelData.rDate.toString(),
+      //                   style: const TextStyle(
+      //                       color: Colors.black, fontSize: 16)),
+      //             ],
+      //           ),
+      //         ),
+      //       ),
+      //     );
+      //   },
+      // ),
+      fallback: (context) =>
+      const Center(child: LinearProgressIndicator()),
+    );});
   }
 
   // ignore: non_constant_identifier_names
