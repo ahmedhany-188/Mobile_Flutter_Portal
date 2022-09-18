1bca2c174dd0884cd0bb004f1155047b4c56819b
Omar.Amir <ibnamir97@gmail.com>
2022-09-18 12:50:45 +0200

    add statistics to my request screen ,, add error widget to get direction screen ,, remove some imports from pubspec.yaml file

--- lib/bloc/statistics_bloc/statistics_state.dart
+++ lib/bloc/statistics_bloc/statistics_state.dart
@@ -8,26 +8,26 @@ enum StatisticsEnumStates { initial, success, failed }
 
 class StatisticsInitial extends Equatable {
   final StatisticsEnumStates statisticsStates;
-  final List<Statistics> statisicsList;
+  final List<Statistics> statisticsList;
 
   const StatisticsInitial({
     this.statisticsStates = StatisticsEnumStates.initial,
-    this.statisicsList = const <Statistics>[],
+    this.statisticsList = const <Statistics>[],
   });
 
   StatisticsInitial copyWith({
     StatisticsEnumStates? statisticsStates,
-    List<Statistics>? statisicsList,
+    List<Statistics>? statisticsList,
   }) {
     return StatisticsInitial(
       statisticsStates: statisticsStates ?? this.statisticsStates,
-      statisicsList: statisicsList ?? this.statisicsList,
+      statisticsList: statisticsList ?? this.statisticsList,
     );
   }
 
   @override
   List<Object> get props => [
         statisticsStates,
-        statisicsList,
+        statisticsList,
       ];
 }
