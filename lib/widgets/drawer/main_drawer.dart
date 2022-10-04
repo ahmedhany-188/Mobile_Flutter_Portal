595af299ce5fd699bd3221d3a5ea0c347d7d8b40
Ahmed.Elghandour <elghandourahmed@gmail.com>
2022-10-04 18:11:07 +0200

    commit update my request in main drawer

--- lib/widgets/drawer/main_drawer.dart
+++ lib/widgets/drawer/main_drawer.dart
@@ -481,7 +481,7 @@ class MainDrawer extends StatelessWidget {
                     ),
                     buildListTile(
                       'My Requests',
-                      Icons.request_page,
+                      Icons.file_copy,
                       () {
                         Navigator.of(context)
                             .popAndPushNamed(MyRequestsScreen.routeName);
