41b340fb4cc46dd16d6c810010dd73543ac9af24
Omar.Amir <ibnamir97@gmail.com>
2022-11-22 16:15:36 +0200

    project detail update some design

--- lib/screens/projects_portfolio/projects_details_screen.dart
+++ lib/screens/projects_portfolio/projects_details_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:hassanallamportalflutter/data/models/get_location_model/location_data.dart';
 import 'package:hassanallamportalflutter/widgets/background/custom_background.dart';
 
+import '../../constants/colors.dart';
 import '../../constants/google_map_api_key.dart';
 import '../../widgets/map/open_map.dart';
 
@@ -27,30 +28,56 @@ class ProjectDetailsScreen extends StatelessWidget {
                 actions: [
                   if (locationData.latitude.toString().contains('.') ||
                       locationData.longitude.toString().contains('.'))
-                    IconButton(
-                        onPressed: () {
-                          openMap(locationData.latitude ?? 0,
-                              locationData.longitude ?? 0);
-                        },
-                        icon: const Icon(
-                          Icons.directions,
-                          // color: ConstantsColors.bottomSheetBackgroundDark,
-                        ))
+                    Container(
+                      margin: const EdgeInsets.only(right: 10),
+                      decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              ConstantsColors.backgroundStartColor,
+                              ConstantsColors.backgroundEndColor,
+                            ],
+                          )),
+                      child: IconButton(
+                          splashRadius: 1,
+                          onPressed: () =>
+                            openMap(locationData.latitude ?? 0,
+                                locationData.longitude ?? 0),
+                          icon: const Icon(
+                            Icons.directions,
+                          )),
+                    )
                 ],
                 leading: IconButton(
                   onPressed: () => Navigator.of(context).pop(),
-                  icon: const Icon(
-                    Icons.arrow_back_outlined,
+                  icon: Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            ConstantsColors.backgroundStartColor,
+                            ConstantsColors.backgroundEndColor,
+                          ],
+                        )),
+                    child: const Icon(
+                      Icons.arrow_back_outlined,
+                    ),
                   ),
                 ),
                 centerTitle: true,
                 backgroundColor: Colors.transparent,
                 elevation: 0,
-                expandedHeight: 300,
+                expandedHeight: MediaQuery.of(context).size.height * 0.30,
+                stretch: true,
                 pinned: true,
-                // snap: true,
-                // floating: true,
-                flexibleSpace:  FlexibleSpaceBar(
+                snap: true,
+                floating: true,
+                flexibleSpace: FlexibleSpaceBar(
                   title: Text(
                     locationData.projectName ?? "Not Defined",
                   ),
@@ -69,7 +96,7 @@ class ProjectDetailsScreen extends StatelessWidget {
                 delegate: SliverChildListDelegate([
                   const Padding(padding: EdgeInsets.all(10)),
                   // const SizedBox(
-                  //   height: 1000,
+                  //   height: 500,
                   // ),
                   Padding(
                     padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
