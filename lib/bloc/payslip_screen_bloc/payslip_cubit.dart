1ab8892ca25bb398323c7127f4775ee8107f6764
Ahmed.hanymostafa <ghazal.developing.team@gmail.com>
2023-03-22 16:24:52 +0200

    Commit - Payslip reset screen - Payslip with three months

--- lib/bloc/payslip_screen_bloc/payslip_cubit.dart
+++ lib/bloc/payslip_screen_bloc/payslip_cubit.dart
@@ -2,8 +2,8 @@
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:bloc/bloc.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:hassanallamportalflutter/data/helpers/download_pdf.dart';
 import 'package:hassanallamportalflutter/data/repositories/payslip_repository.dart';
@@ -17,7 +17,7 @@ part 'payslip_state.dart';
 class PayslipCubit extends Cubit<PayslipState> {
 
   final Connectivity connectivity = Connectivity();
-  PayslipCubit(this._payslipRepository) : super(PayslipInitialState());
+  PayslipCubit(this._payslipRepository) : super(PayslipState());
 
 
   final PayslipRepository _payslipRepository;
@@ -26,7 +26,9 @@ class PayslipCubit extends Cubit<PayslipState> {
     // EasyLoading.init();
 
     // EasyLoading.showSuccess(status)
-    emit(PayslipLoadingState());
+    emit(state.copyWith(
+      payslipDataEnumStates: PayslipDataEnumStates.loading,));
+
         try {
           var connectivityResult = await connectivity.checkConnectivity();
             if (connectivityResult == ConnectivityResult.wifi ||
@@ -38,30 +40,129 @@ class PayslipCubit extends Cubit<PayslipState> {
                   .isAbsolute;
 
               // downloadPdf(response);
-              _validURL ? downloadPdf(user,response) : emit(
-                  PayslipErrorState(
-                      response));
+              _validURL ? downloadPdf(user,response) :
+                  emit(state.copyWith(
+              payslipDataEnumStates: PayslipDataEnumStates.failed,
+                  error: response));
             }else {
-            emit(PayslipErrorState("No internet Connection"));
+            emit(state.copyWith(
+              payslipDataEnumStates: PayslipDataEnumStates.failed,
+            error: "No internet Connection"));
           }
         } catch (e) {
           if (kDebugMode) {
             print(e);
           }
-          emit(PayslipErrorState(e.toString()));
+          emit(state.copyWith(
+              payslipDataEnumStates: PayslipDataEnumStates.failed,
+              error: e.toString()));
         }
   }
+
+  void getPayslipAvailableMonths(User user,String password) async {
+    // EasyLoading.init();
+    // EasyLoading.showSuccess(status)
+    emit(state.copyWith(
+      payslipDataEnumStates: PayslipDataEnumStates.loading,));
+    try {
+      var connectivityResult = await connectivity.checkConnectivity();
+      if (connectivityResult == ConnectivityResult.wifi ||
+          connectivityResult == ConnectivityResult.mobile) {
+        if (password.trim().isEmpty) {
+          emit(state.copyWith(
+              payslipDataEnumStates: PayslipDataEnumStates.failed,
+              error: "Invalid Password"));
+        } else {
+          final response = await _payslipRepository.getPayslipAvailableMonths(
+              user.email, password);
+            String months = response;
+            List<String> monthsList = months.split("-").where((
+                element) => element.toString().trim().isNotEmpty).toList();
+            try{
+              monthsList.map(int.parse).toList();
+              monthsList.isNotEmpty ? emit(state.copyWith(
+                payslipDataEnumStates: PayslipDataEnumStates.success,
+                months: monthsList, response: "Success",
+              )) : emit(state.copyWith(
+                  payslipDataEnumStates: PayslipDataEnumStates.failed,
+                  error: "No data found"));
+            }catch(e){
+              emit(state.copyWith(
+                  payslipDataEnumStates: PayslipDataEnumStates.failed,
+                  error: response));
+            }
+        }
+      } else {
+        emit(state.copyWith(
+            payslipDataEnumStates: PayslipDataEnumStates.failed,
+            error: "No internet Connection"));
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+        emit(state.copyWith(
+            payslipDataEnumStates: PayslipDataEnumStates.failed,
+            error: e.toString()));
+      }
+    }
+  }
+
+  void getPayslipByMonth(User user,String password, String month) async {
+    // EasyLoading.init();
+    // EasyLoading.showSuccess(status)
+    emit(state.copyWith(
+      payslipDataEnumStates: PayslipDataEnumStates.loading,));
+    try {
+      var connectivityResult = await connectivity.checkConnectivity();
+      if (connectivityResult == ConnectivityResult.wifi ||
+          connectivityResult == ConnectivityResult.mobile) {
+        final response = await _payslipRepository.getPayslipByMonth(
+            user.email, password, month);
+        if (password.trim().isEmpty) {
+          emit(state.copyWith(
+              payslipDataEnumStates: PayslipDataEnumStates.failed,
+              error: "Invalid Password"));
+        }else{
+          bool _validURL = Uri
+              .parse(response)
+              .isAbsolute;
+          _validURL ? downloadPdf(user,response) :
+          emit(state.copyWith(
+              payslipDataEnumStates: PayslipDataEnumStates.failed,
+              error: response));
+        }
+      }else {
+        emit(state.copyWith(
+            payslipDataEnumStates: PayslipDataEnumStates.failed,
+            error: "No internet Connection"));
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+      emit(state.copyWith(
+          payslipDataEnumStates: PayslipDataEnumStates.failed,
+          error: e.toString()));
+    }
+  }
+
   void downloadPdf(User user,String link){
-    emit(PayslipDownloadState("Downloading..."));
+    emit(state.copyWith(
+        payslipDataEnumStates: PayslipDataEnumStates.download,
+        response: "Downloading..."));
     List<String> split = link.split("/");
     int monthNumber  = int.parse(split[split.length-2]);
     var monthName = DateFormat('MMMM').format(DateTime(0, monthNumber));
     // downloadPdfHelper.requestDownload(link, "Payslip-"+ monthName+".pdf");
 
     DownloadPdfHelper(fileName: "Payslip-${user.userHRCode}-$monthName.pdf",fileUrl: link,success: (){
-      emit(PayslipSuccessState("Finished"));
+      emit(state.copyWith(
+          payslipDataEnumStates: PayslipDataEnumStates.success,
+          response: "Finished"));
     },failed: (){
-      emit(PayslipErrorState("Error"));
+      emit(state.copyWith(
+          payslipDataEnumStates: PayslipDataEnumStates.failed,
+          error: "Error"));
     }).download();
 
   }
