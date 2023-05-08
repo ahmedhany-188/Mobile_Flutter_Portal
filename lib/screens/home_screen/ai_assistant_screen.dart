f6a2a1c1acaaf5fadda0310a16205e8a62d4d007
ahmedhany-188 <ahmedhany00_k.w.k@outlook.sa>
2023-05-08 12:05:39 +0300

    Commit
    -  test for AI screen page

--- lib/screens/home_screen/ai_assistant_screen.dart
+++ lib/screens/home_screen/ai_assistant_screen.dart
@@ -4,7 +4,8 @@
 // import 'package:hassanallamportalflutter/bloc/helper_bot_screen_bloc/portal_assistant_cubit.dart';
 // import 'package:hassanallamportalflutter/widgets/background/custom_background.dart';
 // import 'package:intl/intl.dart';
-//////////........////////////......////////////////////
+
+// new update 5-8-2023
 // // ignore: must_be_immutable
 // class AIAssitantScreen extends StatelessWidget {
 //   AIAssitantScreen({Key? key}) : super(key: key);
