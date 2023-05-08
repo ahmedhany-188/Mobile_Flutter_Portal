819f7841646aedc31dd5d9578adf4eee7a6219ab
ahmedhany-188 <ahmedhany00_k.w.k@outlook.sa>
2023-05-08 10:46:19 +0300

    Commit
    - the page of AI assistant screen

--- lib/screens/home_screen/ai_assistant_screen.dart
+++ lib/screens/home_screen/ai_assistant_screen.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
-import 'package:hassanallamportalflutter/bloc/helper_bot_screen_bloc/portal_assistant_cubit.dart';
-import 'package:hassanallamportalflutter/widgets/background/custom_background.dart';
-import 'package:intl/intl.dart';
-//
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:flutter_easyloading/flutter_easyloading.dart';
+// import 'package:hassanallamportalflutter/bloc/helper_bot_screen_bloc/portal_assistant_cubit.dart';
+// import 'package:hassanallamportalflutter/widgets/background/custom_background.dart';
+// import 'package:intl/intl.dart';
+//////////////////////////////////////////
 // // ignore: must_be_immutable
 // class AIAssitantScreen extends StatelessWidget {
 //   AIAssitantScreen({Key? key}) : super(key: key);
